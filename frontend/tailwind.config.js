8622360a576459411d6bb4ecc43c2d74b7174db4
Soham Das <dassoham345@gmail.com>
2024-09-28 19:48:50 +0530

    project done boss

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Ensure this path covers all your source files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
